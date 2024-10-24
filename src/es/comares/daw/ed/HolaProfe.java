8981cb49ec6c6f480f9531427ffc549db1209eb1
manologomez123 <145774876+manologomez123@users.noreply.github.com>
2024-10-24 08:46:38 +0200

    Update HolaProfe.java

--- src/es/comares/daw/ed/HolaProfe.java
+++ src/es/comares/daw/ed/HolaProfe.java
@@ -5,6 +5,7 @@ public class HolaProfe {
 	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es Lucas Curiel");
 		System.out.println("Hola mi nombre es Natanael Gómez");
+		System.out.println("Buenas, soy Manolo. Ahora debéis obtener los cambios desde Eclipse. No olividéis documentarlo con capturas de pantalla ");
 
 	}
 
