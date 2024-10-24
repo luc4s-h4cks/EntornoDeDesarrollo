9e4ada3fcbf9085fd9e7502e947ab130240598e5
h4ckx0r <natanagoor@gmail.com>
2024-10-24 08:45:44 +0200

    Se añade un syso con el nombre Natanael

--- src/es/comares/daw/ed/HolaProfe.java
+++ src/es/comares/daw/ed/HolaProfe.java
@@ -4,6 +4,7 @@ public class HolaProfe {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es Lucas Curiel");
+		System.out.println("Hola mi nombre es Natanael Gómez");
 
 	}
 
